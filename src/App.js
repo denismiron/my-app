77028150a4423dcfaaf9eb2796f8ec5be5ad61f3
denismiron <denisbvb0@gmail.com>
2023-12-06 10:26:36 +0300

    posts add, remove

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react';
+import './styles/App.css';
+import PostList from './components/PostList';
+import PostForm from './components/PostForm';
 
 function App() {
+  const [posts, setPosts] = useState([
+      {id:1, title: 'Javascript', body: 'Description'},
+      {id:2, title: 'Javascript 2', body: 'Description'},
+      {id:3, title: 'Javascript 3', body: 'Description'},
+      {id:4, title: 'Javascript 4', body: 'Description'},
+      {id:5, title: 'Javascript 5', body: 'Description'},
+  ])
+
+  const createPost = (newPost) => {
+      setPosts([...posts, newPost])
+  }
+  
+  const removePost = (post) => {
+      setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostForm create={createPost}/>
+      {posts.length
+        ? 
+        <PostList  posts={posts} title="Список постов" remove={removePost}/>
+        : 
+        <h1 style={{textAlign:'center', }}>
+          Посты не найдены!
+        </h1>
+      }
+
     </div>
   );
 }
