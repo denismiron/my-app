a4f92d1f53f84ec1c497aad0215df9b972fb1ae6
denismiron <denisbvb0@gmail.com>
2023-12-11 21:53:29 +0300

    pagination

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 export default class PostService {
-  static async getAll() {
-    try {
-      const responce = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      return responce.data;
-    } catch (e) {
-      console.log(e);
-    }
+  static async getAll(limit = 10, page = 1) {
+    const responce = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts",
+      {
+        params: {
+          _limit: limit,
+          _page: page,
+        },
+      }
+    );
+    return responce;
   }
 }
--- src/App.js
+++ src/App.js
@@ -8,16 +8,29 @@ import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Spinner from "./components/UI/spinner/spinner";
+import { useFetching } from "./hooks/useFetching";
+import { getPagesArray, getPageCount } from "./utils/pages";
+import Pagination from './components/UI/pagination/pagination';
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostLoading, setIsPostLoading] = useState(false);
+
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
+    const responce = await PostService.getAll(limit, page);
+    setPosts(responce.data);
+    const totalCount = responce.headers["x-total-count"];
+    setTotalPages(getPageCount(totalCount, limit));
+  });
 
   useEffect(() => {
-    fetchPosts();
+    fetchPosts(limit, page);
   }, []);
 
   const createPost = (newPost) => {
@@ -25,44 +38,36 @@ function App() {
     setModal(false);
   };
 
-  async function fetchPosts() {
-    setIsPostLoading(true);
-    const posts = await PostService.getAll();
-    setPosts(posts);
-    setIsPostLoading(false);
-  }
-
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
+  const changePage = (page) => {
+      setPage(page);
+      fetchPosts(limit, page);
+  } 
+
   return (
     <div className="App">
-      <MyButton style={{ marginTop: "25px" }} onClick={() => setModal(true)}>
-        Создать
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
+      <MyButton style={{ marginTop: "25px" }} onClick={() => setModal(true)}>Создать</MyButton>
+      <MyModal visible={modal} setVisible={setModal}><PostForm create={createPost} /></MyModal>
       <hr style={{ margin: "15px 0" }} />
-      <PostFilter filter={filter} setFilter={setFilter} />
-      {isPostLoading ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "50px",
-          }}
-        >
-          <Spinner />
-        </div>
-      ) : (
-        <PostList
-          posts={sortedAndSearchedPosts}
-          title="Список постов"
-          remove={removePost}
-        />
-      )}
+      <PostFilter 
+        filter={filter} 
+        setFilter={setFilter} 
+      />
+      {postError && 
+        <h1>Произошла ошибка ${postError}</h1>
+      }
+      {isPostsLoading 
+        ? <div style={{display: "flex", justifyContent: "center", marginTop: "50px",}}><Spinner /></div> 
+        : <PostList posts={sortedAndSearchedPosts} title="Список постов" remove={removePost}/>
+      }
+      <Pagination  
+        page={page} 
+        changePage={changePage} 
+        totalPages={totalPages}
+      />
     </div>
   );
 }
