1efc0c96fd7afc4bdb2b1c5a6f84db8031441327
denismiron <denisbvb0@gmail.com>
2023-12-12 22:02:25 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,75 +1,28 @@
 import React, { useEffect, useState } from "react";
 import "./styles/App.css";
-import PostList from "./components/PostList";
-import PostForm from "./components/PostForm";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./components/UI/modal/MyModal";
-import MyButton from "./components/UI/button/MyButton";
-import { usePosts } from "./hooks/usePosts";
-import PostService from "./API/PostService";
-import Spinner from "./components/UI/spinner/spinner";
-import { useFetching } from "./hooks/useFetching";
-import { getPagesArray, getPageCount } from "./utils/pages";
-import Pagination from './components/UI/pagination/pagination';
+import { BrowserRouter, Link, Route, Routes, Redirect, Switch, Navigate } from "react-router-dom";
+import About from './pages/About';
+import Posts from './pages/Posts';
+import { SwitchTransition } from 'react-transition-group';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [filter, setFilter] = useState({ sort: "", query: "" });
-  const [modal, setModal] = useState(false);
-  const [totalPages, setTotalPages] = useState(0);
-  const [limit, setLimit] = useState(10);
-  const [page, setPage] = useState(1);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
-    const responce = await PostService.getAll(limit, page);
-    setPosts(responce.data);
-    const totalCount = responce.headers["x-total-count"];
-    setTotalPages(getPageCount(totalCount, limit));
-  });
-
-  useEffect(() => {
-    fetchPosts(limit, page);
-  }, []);
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  };
-
-  const removePost = (post) => {
-    setPosts(posts.filter((p) => p.id !== post.id));
-  };
-
-  const changePage = (page) => {
-      setPage(page);
-      fetchPosts(limit, page);
-  } 
-
   return (
-    <div className="App">
-      <MyButton style={{ marginTop: "25px" }} onClick={() => setModal(true)}>Создать</MyButton>
-      <MyModal visible={modal} setVisible={setModal}><PostForm create={createPost} /></MyModal>
-      <hr style={{ margin: "15px 0" }} />
-      <PostFilter 
-        filter={filter} 
-        setFilter={setFilter} 
-      />
-      {postError && 
-        <h1>Произошла ошибка ${postError}</h1>
-      }
-      {isPostsLoading 
-        ? <div style={{display: "flex", justifyContent: "center", marginTop: "50px",}}><Spinner /></div> 
-        : <PostList posts={sortedAndSearchedPosts} title="Список постов" remove={removePost}/>
-      }
-      <Pagination  
-        page={page} 
-        changePage={changePage} 
-        totalPages={totalPages}
-      />
-    </div>
+    <BrowserRouter>
+      {/* <Navbar/> */}
+      <div className="navbar">
+        <div className="navbar__links">
+          <Link to="/about">О сайте</Link>
+          <Link to="/posts">Посты</Link>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="*" element={<Navigate replace to="/error" />} /> ?
+      </Routes>
+    </BrowserRouter>
   );
+
 }
 
 export default App;
---
+++ src/pages/Posts.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
+import PostList from "../components/PostList";
+import PostForm from "../components/PostForm";
+import PostFilter from "../components/PostFilter";
+import MyModal from "../components/UI/modal/MyModal";
+import MyButton from "../components/UI/button/MyButton";
+import { usePosts } from "../hooks/usePosts";
+import PostService from "../API/PostService";
+import Spinner from "../components/UI/spinner/spinner";
+import { useFetching } from "../hooks/useFetching";
+import { getPagesArray, getPageCount } from "../utils/pages";
+import Pagination from "../components/UI/pagination/pagination";
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching(
+    async (limit, page) => {
+      const responce = await PostService.getAll(limit, page);
+      setPosts(responce.data);
+      const totalCount = responce.headers["x-total-count"];
+      setTotalPages(getPageCount(totalCount, limit));
+    }
+  );
+
+  useEffect(() => {
+    fetchPosts(limit, page);
+  }, []);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+    setModal(false);
+  };
+
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  const changePage = (page) => {
+    setPage(page);
+    fetchPosts(limit, page);
+  };
+
+  return (
+    <div className="App">
+      <MyButton style={{ marginTop: "25px" }} onClick={() => setModal(true)}>
+        Создать
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+      <hr style={{ margin: "15px 0" }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      {postError && <h1>Произошла ошибка ${postError}</h1>}
+      {isPostsLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "50px",
+          }}
+        >
+          <Spinner />
+        </div>
+      ) : (
+        <PostList
+          posts={sortedAndSearchedPosts}
+          title="Список постов"
+          remove={removePost}
+        />
+      )}
+      <Pagination page={page} changePage={changePage} totalPages={totalPages} />
+    </div>
+  );
+}
+
+export default Posts;
