bd1f55b4b16784e2851fd20cbfa3f597f0f463d6
denismiron <denisbvb0@gmail.com>
2023-12-13 15:02:45 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { useEffect, useState } from "react";
 import "./styles/App.css";
-import { BrowserRouter, Link, Route, Routes, Redirect, Switch, Navigate } from "react-router-dom";
-import About from './pages/About';
-import Posts from './pages/Posts';
-import { SwitchTransition } from 'react-transition-group';
+import { BrowserRouter } from "react-router-dom";
+
+import Navbar from './components/UI/navbar/navbar';
+import AppRouter from './components/AppRouter';
+
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Navbar/> */}
-      <div className="navbar">
-        <div className="navbar__links">
-          <Link to="/about">О сайте</Link>
-          <Link to="/posts">Посты</Link>
-        </div>
-      </div>
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="*" element={<Navigate replace to="/error" />} /> ?
-      </Routes>
+      <Navbar/>
+      <AppRouter/>
     </BrowserRouter>
   );
 
---
+++ src/components/AppRouter.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import About from "../pages/About";
+import Posts from "../pages/Posts";
+import Error from "../pages/Error";
+import {
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+const AppRouter = () => {
+    return (
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/error" element={<Error />} />
+        <Route path="*" element={<Navigate replace to="/error" />} />
+      </Routes>
+    );
+};
+
+export default AppRouter;
